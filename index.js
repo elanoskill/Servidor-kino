8d82f0b42f8f104f798e05338af62dc3c7a80149
Elano Bernardo <elanobernardo1@gmail.com>
2026-05-10 12:39:27 +0100

    atualizacao do index

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import express from "express";
 import router from "./src/router/privado.js";
 import publica from "./src/router/publica.js";
 const app = express();
-const PORTA = precess.env.PORT || 3000;
+const PORTA = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/",router)
 app.use("/index",publica);
 
 
 
-module.exports =app;
+export default app
