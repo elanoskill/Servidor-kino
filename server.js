11b366c6f824b33c179e8f8081cbd5a649854c18
Elano Bernardo <elanobernardo1@gmail.com>
2026-05-10 12:29:25 +0100

    adicionado a port no server

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
+import 'dotevn/config';
 import express from "express";
 import router from "./src/router/privado.js";
 import publica from "./src/router/publica.js";
 const app = express();
-const PORTA = 3000;
+const PORTA = precess.env.PORT || 3000;
 app.use(express.json());
 app.use("/",router)
 app.use("/index",publica);
 
 
 
-app.listen(PORTA, () => console.log("O servidor esta no ar"));
+module.exports =app;
